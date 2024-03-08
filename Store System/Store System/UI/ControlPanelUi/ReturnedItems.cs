f0f6e401997dc587b12cae81d51c36fbf958653a
Mohamed Fadel <fadelmohamed445@gmail.com>
2024-03-08 15:50:44 +0200

    Sale&Buy&Returned&Shiftlock Bills

---
+++ Store System/Store System/UI/ControlPanelUi/ReturnedItems.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Store_System.UI.ControlPanelUi
+{
+    public partial class ReturnedItems : UserControl
+    {
+        public ReturnedItems()
+        {
+            InitializeComponent();
+        }
+    }
+}
